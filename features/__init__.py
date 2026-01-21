77f096e19bbf42a9f97da14946aee5e66e54a290
MaiHanh-Tech <hanhltm80@gmail.com>
2026-01-21 09:31:44 +0700

    Update __init__.py

--- features/__init__.py
+++ features/__init__.py
@@ -1,1 +1,15 @@
+"""
+features package
+"""
 
+# Root-level features (không nằm trong subfolder)
+from .history_feature import HistoryFeature
+from .cfo_feature import CFOFeature
+
+# Subfolder features
+try:
+    from .weaver import WeaverFeature
+except ImportError:
+    WeaverFeature = None
+
+__all__ = ['WeaverFeature', 'HistoryFeature', 'CFOFeature']
